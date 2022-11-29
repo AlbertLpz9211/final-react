9699784fb62330022b880705af2b93f4d1f84282
AlbertLpz9211 <alberto.lopez_g@outlook.com>
2022-11-29 02:17:46 -0600

    add img to Root

--- src/pages/RootPage.jsx
+++ src/pages/RootPage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { getProductThunk } from "../store/slices/products.slice";
 
 const RootPage = () => {
-
   const dispatch = useDispatch();
-  
+  const products = useSelector((state) => state.producs);
+
   useEffect(() => {
     dispatch(getProductThunk());
   }, []);
@@ -14,6 +15,13 @@ const RootPage = () => {
   return (
     <div>
       <h1>componente raiz</h1>
+      {products.map((productsItems) => (
+        <li key={productsItems.id}>
+          <Link to={`/product/${productsItems.id}`}>{productsItems.title}</Link>
+          <br />
+          <img src={productsItems.productImgs[0]} alt="" />
+        </li>
+      ))}
     </div>
   );
 };
