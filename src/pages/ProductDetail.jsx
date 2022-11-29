82ec92bcdd01492e227a4558b7aee8c50ff276f5
AlbertLpz9211 <alberto.lopez_g@outlook.com>
2022-11-29 02:55:53 -0600

    fill the PRoductDetail component, for get especific product

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getProductThunk } from "../store/slices/products.slice";
 
 const ProductDetail = () => {
-    return (
-        <div>
-            <h1>componente de detalle de productos</h1>
-        </div>
-    );
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getProductThunk());
+  }, []);
+
+  const productsList = useSelector((state) => state.producs);
+
+  const product = productsList.find(
+    (productItem) => productItem.id === Number(id)
+  );
+  console.log(product);
+  return (
+    <div>
+      <h1>{product?.title}</h1>
+      <img src={product?.productImgs[0]} alt="" />
+    </div>
+  );
 };
 
 export default ProductDetail;
